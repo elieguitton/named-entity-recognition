579f6ada1310821bc83b35d4563897ab72e9be38
Elie Guitton <80695857+elieguitton@users.noreply.github.com>
2022-09-28 15:16:44 +0200

    Create textProcessing.py
    Fonctions pour traiter le texte récupérer

---
+++ textProcessing.py
@@ -0,0 +1,15 @@
+import glob
+import json
+import re
+import nltk
+
+from nltk.corpus import stopwords
+
+
+# 1 - On enlève la ponctuation
+def enlever_ponctuation(chaine):
+    return re.sub(r"[^\w\s]", "", chaine)
+
+# 2 - On enlève les mots vides
+def enlever_mots_vides(chaine):
+    return [mot for mot in chaine if mot not in stopwords.words("french")]
